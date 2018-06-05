18686fb71129669bfe5c4a870cfa8556dfa37ec7
Sean Clifford <sclifford6262@gmail.com>
2018-06-05 16:26:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function createIssue() {
 function showResults(json) {
 }
 
-/*function forkRepo() {
+/*
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
   fetch(`https://api.github.com/repos/${repo}/forks`, {
