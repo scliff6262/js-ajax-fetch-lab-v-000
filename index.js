efcc5f81130d909603cc58b9e3df900e9851bad1
Sean Clifford <sclifford6262@gmail.com>
2018-06-05 15:33:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   const token = '29fe5c75d8b39e72f502dc4719212013e693af3e'
-  const apiLink = "https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks"
   //use fetch to fork it!
-  fetch("https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks", {
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
     method: "POST",
     headers: {
       Authorization: `token ${token}`
